56047f40bd00cf56cd4d9fa891f1c31b59d2f62c
retroPrograms <mcarvalho_au@hotmail.com>
2021-04-29 22:44:31 +1000

    Added console.asm (2)

--- console.asm
+++ console.asm
@@ -1,53 +1,110 @@
-OpenLib		=       -30-378	
-closelib	=	-414
-ExecBase	=	4
-
-Open		=	-30
-Close		=	-30-6
-Write		=	-48
-IoErr		=	-132
-mode_old	=	1005
-alloc_abs	=	-$cc
+MoveScreen 	= -162
+OpenScreen	= -198
+CloseScreen	= -66
+CloseLibrary	= -414
+OpenLib	= -408
+ExecBase	= 4
+Write		= -48
+mode_old	= 1005
+joy2		= $dff00c
+fire 		= $bfe001
 
 run:
-	bsr	init
-	bsr	test
-	nop
-	bra	qu
-	
-test:
-	move.l	#title,d0
-	bsr	pmsg
-	bsr	pcrlf
-	bsr	pcrlf
+	bsr	openint
+	bsr	scropen
+       bsr Init
 
 waitmouse:
        btst #6,$bfe001
        bne waitmouse
+       
+       bsr scrclose
+       bsr closeint
        rts
-	
-	rts
-	
-init:
-	move.l	ExecBase,a6
+
+Init:              
+       move.l	ExecBase,a6
 	lea	dosname(pc),a1 ;dosname(pc),al
 	moveq	#0,d0
 	jsr	openlib(a6)
 	move.l	d0,dosbase
-	beq	error
+	;beq	error
 	
 	lea 	consolname(pc),a1
 	move.l	#mode_old,d0
-	bsr	openfile
-	beq	error
+	;bsr	openfile
+	;beq	error
 	move.l	d0,conhandle
+       rts
+	
+	move	joy2,d6
+loop:
+	tst.b	fire
+	bpl	ende
+	move	joy2,d0
+	sub	d6,d0
+	cmp	#$0100,d0
+	bne	noup
+	move.l	#-1,d1
+	bsr	 scrmove
+	bra loop
+	
+noup:
+	cmp	#$0001,d0
+	bne	 loop
+	move.l 	#1,d1
+	bsr	scrmove
+	bra	loop
+	
+ende:
+	bsr     scrclose
+	bsr	closeint
+	rts
+	
+openint:
+	move.l	ExecBase,a6
+	lea 	IntName,a1
+	jsr	OpenLib(a6)
+	move.l	d0,intbase
+	rts
+
+closeint:
+	move.l  execbase,a6
+	move.l	intbase,a1
+	jsr	CloseLibrary(a6)
+	rts
+	
+scropen:
+	move.l intbase,a6
+	lea screen_defs,a0
+	jsr OpenScreen(a6)
+	move.l	d0,screenhd
+	rts
+	
+scrclose:
+	move.l	intbase,a6
+	move.l 	screenhd,a0
+	jsr CloseScreen(a6)
+	rts
+	
+scrmove:
+	move.l	intbase,a6
+	move.l	screenhd,a0
+	clr.l	d0
+	jsr MoveScreen(a6)
+	rts	
+
+Out:
 	rts
 
 pmsg:
+        
 	movem.l	d0-d7/a0-a6,-(sp)
+        
 	move.l	d0,a0
 	move.l	a0,d2
 	clr.l	d3
+       move.l	#title1,d0
 ploop:
 	tst.b	(a0)+
 	beq	pmsg2
@@ -60,61 +117,52 @@ pmsg2:
 	movem.l	(sp)+,d0-d7/a0-a6
 	rts
 	
-pcrlf:
-	move	#10,d0
-	bsr	pchar
-	move	#13,d0
-pchar:
-	movem.l	d0-d7/a0-a6,-(sp)
-	move.l	conhandle,d1
-pch1:
-	lea	outline,a1
-	move.b	d0,(a1)
-	move.l	a1,d2
-	move.l	#1,d3
-	move.l	dosbase,a6
-	jsr	write(a6)
-	movem.l	(sp)+,d0-d7/a0-a6
-	rts
 	
-error:
-	move.l	dosbase,a6
-	jsr	IoErr(a6)
-	move.l	d0,d5
-	move.l	#-1,d7
-qu:
-	move.l	conhandle,d1
-	move.l	dosbase,a6
-	jsr	close(a6)
-	move.l	dosbase,a1
-	move.l 	ExecBase,a6
-	jsr	closelib(a6)
-	
-openfile:
-	move.l	a1,d1
-	move.l	d0,d2
-	move.l	dosbase,a6
-	jsr	open(a6)
-	tst.l	d0
-	rts
-	
-dosname:
-	dc.b	 'dos.library',0,0
-	even
+screen_defs:
+
+x_pos:		dc.w	0
+y_pos:		dc.w	0
+width:		dc.w	320
+height:		dc.w	200
+depth:		dc.w	2
+detail_pen:	dc.b	1
+block_pen:	dc.b	3
+view_modes:	dc.w	2
+screen_type:	dc.w	15
+font:		dc.l	0
+title:		dc.l	sname
+gadgets:	dc.l	0
+bitmap:		dc.l	0
+
+
+intbase:	dc.l	0
+
+screenhd:	dc.l	0
+
+IntName:
+	dc.b 'intuition.library',0
+        even
+sname:
+	dc.b 'Our Screen',0 
+        even
+
+DOS          dc.b    'dos.library',0
+HelloWorld   dc.b    'Hello World!',$A,0
+
 dosbase:
 	dc.l	0
 	
+
+conhandle:
+	dc.l 0
+
+dosname:
+	dc.b	 'dos.library',0,0
+	even
+
 consolname:
 	dc.b 'CON:0/100/640/100/** CLI-Test **',0
 	even
-conhandle:
-	dc.l 0
-title2:
-	dc.b '** Welcome to this program! **'
-outline2:
-	dc.w 0	
 
-title:
-        dc.b 'Soc. And you remember how we said that the children of the good parents were to be educated, and the children of the bad secretly dispersed among the inferior citizens; and while they were all growing up the rulers were to be on the look-out, and to bring up from below in their turn those who were worthy, and those among themselves who were unworthy were to take the places of those who came up?'
-outline:
-        dc.w 0
+title1:
+	dc.b '** Hello World! **'
